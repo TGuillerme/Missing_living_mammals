08e8ec3b7589a43873907415143936eaae8462cd
TGuillerme <guillert@tcd.ie>
2015-01-14 14:28:47 +0000

    Extracting living taxa FUN

--- Functions/extract.names.R
+++ Functions/extract.names.R
@@ -7,22 +7,170 @@
 #guillert(at)tcd.ie - 13/01/2015
 ##########################
 
-extract.names<-function(matrix, references) {
 
-    #DEBUG
-    matrix<-read.nexus.file("Matrices/Gl1988-GH.nex")
-    tree_list<-read.table("Taxon_References/FritzTree_taxa.txt", header=F, stringsAsFactors=F)
-    WR_list<-read.csv("Taxon_References/WilsonReederMSW.csv", header=T, stringsAsFactors=F)
-    Paleo_list<-read.csv("Taxon_References/pdb_taxa.csv", header=T, stringsAsFactors=F)
-    Ref_list<-list(tree_list, WR_list, Paleo_list)
-    names(Ref_list)<-c("tree", "WR", "Paleo")
+#FUNCTIONS
+#functions for matching one name of the matrix
+#Extracting the first match
+find.match<-function(matrix_taxon, Ref_list_element) {
+    options(warn=-1)
+    to_match<-grep(matrix_taxon, as.character(unlist(Ref_list_element)), ignore.case=TRUE, value=TRUE)
+    if(length(to_match) == 0) {
+        match_found<-FALSE
+    } else {
+        #Making sure it matches exactly
+        match_test<-regexec(matrix_taxon, to_match[1], ignore.case=TRUE)
+        #Check if the number of matching characters in to_match is the same length as the ones in matrix_taxon (i.e. full match)
+        if(attr(match_test[[1]], "match.length")==nchar(to_match[1])) {
+            match_found<-TRUE
+        } else {
+            match_found<-FALSE
+        }
+    }
+    return(match_found)
+}
 
+#Extracting the number of the column containing is.match
+level.column<-function(is_match, Ref_list_element) {
+    level_column<-NA
+    for(n in 1:ncol(Ref_list_element)) {
+        level_column[n]<-match(is_match, as.character(Ref_list_element[,n]))[1]
+    }
+    return(which(!is.na(level_column)))
+}
+
+#Try matching by separating genus and species
+check.split<-function(matrix_taxon, Ref_list_element) {
+    if(length(grep("_", matrix_taxon))==1) {
+        #Genus names
+        genus<-strsplit(matrix_taxon, split="_")[[1]][1]
+        #Species name
+        species<-strsplit(matrix_taxon, split="_")[[1]][2]
+
+        #Is the genus name matching?
+        genus_match<-find.match(genus, Ref_list_element)
+        if(genus_match == TRUE) {
+            #Find the genus name
+            genus_match<-grep(genus, as.character(unlist(Ref_list_element)), ignore.case=TRUE, value=TRUE)[1]
+
+            #Is the species name matching?
+            species_match<-find.match(genus, Ref_list_element)
+            if(species_match == TRUE) {
+                #Find the species name
+                species_match<-grep(species, as.character(unlist(Ref_list_element)), ignore.case=TRUE, value=TRUE)[1]
+            } else {
+                species_match<-NA
+            }
+
+        } else {
+            #No matching
+            genus_match<-NA
+            species_match<-NA
+        }
+
+    } else {
+        #impossible to split
+        genus_match<-NA
+        species_match<-NA
+    }
+
+    return(c(genus_match, species_match))
+}
+
+
+match.taxon<-function(matrix_taxon, Ref_list) {
+    #Checking through the first element of the list (Fritz tree)
+    match_found<-find.match(matrix_taxon, Ref_list[[1]])
+
+    if(match_found==TRUE) {
+        #Saving the information
+        is_living<-TRUE
+        is_source<-names(Ref_list)[1]
+        is_level<-"Species"
+
+    } else {
+        #Checking through the second element of the list (Wilson Reeder list)
+        match_found<-find.match(matrix_taxon, Ref_list[[2]])
+            
+        if(match_found==TRUE) {
+            #Saving the information
+            is_living<-TRUE
+            is_source<-names(Ref_list)[2]
+            is_match<-grep(matrix_taxon, as.character(unlist(Ref_list[[2]])), ignore.case=TRUE, value=TRUE)[1]
+
+            is_level<-names(Ref_list[[2]])[(level.column(is_match, Ref_list[[2]]))]
+        
+        } else {
+
+            #Try to split the genera/species
+            genus_species<-check.split(matrix_taxon, Ref_list[[2]])
+
+            #If genus or genus_species is present, save the information
+            if(!is.na(genus_species[1])) {
+                #Saving the information
+                is_living<-TRUE
+                is_source<-names(Ref_list)[2]
+
+                #Checking the level
+                if(!is.na(genus_species[2])) {
+                    is_level<-"Species"
+                } else {
+                    is_level<-"Genus"
+                }
+
+            } else {
+                #Checking through the third element of the list (Paleo DB)
+                match_found<-find.match(matrix_taxon, Ref_list[[3]])
+
+                if(match_found==TRUE) {
+                    #Saving the information
+                    is_living<-FALSE
+                    is_source<-names(Ref_list)[3]
+                    is_match<-grep(matrix_taxon, as.character(unlist(Ref_list[[3]])), ignore.case=TRUE, value=TRUE)[1]
+
+                    #Checking the level
+                    if(level.column(is_match, Ref_list[[3]])==1) {
+                        #Present in the first column
+                        is_level<-"Genus"
+                    } else {
+                        #Present in the second column
+                        is_level<-"Species"
+                    }
+
+                } else {
+                    #Try to split the genera/species
+                    genus_species<-check.split(matrix_taxon, Ref_list[[3]])
+
+                    #If genus or genus_species is present, save the information
+                    if(!is.na(genus_species[1])) {
+                        #Saving the information
+                        is_living<-FALSE
+                        is_source<-names(Ref_list)[3]
+
+                        #Checking the level
+                        if(!is.na(genus_species[2])) {
+                            is_level<-"Species"
+                        } else {
+                            is_level<-"Genus"
+                        }
+
+                    } else {
+                        #No match at all!
+                        is_living<-NA
+                        is_source<-NA
+                        is_level<-NA
+                    } 
+                }
+            }
+        }
+    }
+
+    #Output
+    return(c(is_living, is_source, is_level))
+}
+
+extract.names<-function(matrix, references, verbose=FALSE) {
     #requirements
     require(ape)
-
-    #Sanitizing
-    #Sanitizing functions
-    source("sanitizing.R")
     
     #matrix
     check.class(matrix, "list", " must be a nexus matrix.\n Use read.nexus.data().")
@@ -34,159 +182,33 @@ extract.names<-function(matrix, references) {
     taxa_list<-names(matrix)
 
     #references
-    check.class(Ref_list, "list", " must be a list of data.frames.")
+    check.class(references, "list", " must be a list of data.frames.")
     #checking each element
-    for(n in 1:length(Ref_list)) {
-        check.class(Ref_list[[n]], "data.frame", " must be a list of data.frames.")
+    for(n in 1:length(references)) {
+        check.class(references[[n]], "data.frame", " must be a list of data.frames.")
     }
     #the list must have names
-    if(is.null(names(Ref_list))) {
+    if(is.null(names(references))) {
         stop("The Reference list must have names provided.")
     }
 
-    #FUNCTION
-    #functions for matching one name of the matrix
+    #verbose
+    check.class(verbose, "logical", " must be logical.")
 
-    #Extracting the first match
-    find.match<-function(matrix_taxon, Ref_list_element) {
-        options(warn=-1)
-        return(grep(matrix_taxon, as.character(unlist(Ref_list_element)), ignore.case=TRUE, value=TRUE))
-    }
+    #EXTRACTING NAMES
 
-    #Extracting the number of the column containing is.match
-    level.column<-function(is_match, Ref_list_element) {
-        level_column<-NA
-        for(n in 1:ncol(Ref_list_element)) {
-        level_column[n]<-match(is_match, as.character(Ref_list_element[,n]))[1]
+    #Building the empty data frame.
+    output<-data.frame("Taxa"=taxa_list, "Characters"=rep(n_characters, length(taxa_list)), "Living"=rep(NA, length(taxa_list)), "Source"=rep(NA, length(taxa_list)), "Tax.level"=rep(NA, length(taxa_list)))
+
+    for (taxon in 1: length(taxa_list)) {
+        match_results<-match.taxon(taxa_list[taxon], references)
+        output$Living[taxon]<-match_results[1]
+        output$Source[taxon]<-match_results[2]
+        output$Tax.level[taxon]<-match_results[3]
+        if(verbose==TRUE) {
+            message('.', appendLF=FALSE)
         }
-        return(which(!is.na(level_column)))
     }
 
-    #Try matching by separating genus and species
-    check.split<-function(matrix_taxon, Ref_list_element) {
-        if(grep("_", matrix_taxon)==1) {
-            #Genus names
-            genus<-strsplit(matrix_taxon, split="_")[[1]][1]
-            #Species name
-            species<-strsplit(matrix_taxon, split="_")[[1]][2]
-
-            #Finding the genus name
-            genus_match<-find.match(genus, Ref_list_element)
-
-            #Finding the species name
-            species_match<-grep(species, Ref_list_element[genus_match,])
-
-        } else {
-            #impossible to split
-            genus_match<-NA
-            species_match<-NA
-        }
-
-        return(c(genus_match, species_match))
-    }
-
-    match.taxon<-function(matrix_taxon, Ref_list) {
-        #Checking through the first element of the list (Fritz tree)
-        is_match<-find.match(matrix_taxon, Ref_list[[1]])[1]
-
-        if(!is.na(is_match)) {
-            #Saving the information
-            is_living<-TRUE
-            is_source<-names(Ref_list)[1]
-            is_level<-"Species"
-
-        } else {
-            #Checking through the second element of the list (Wilson Reeder list)
-            is_match<-find.match(matrix_taxon, Ref_list[[2]])[1]
-            
-            if(!is.na(is_match)) {
-                #Saving the information
-                is_living<-TRUE
-                is_source<-names(Ref_list)[2]
-                is_level<-names(Ref_list[[2]])[(level.column(is_match, Ref_list[[2]]))]
-            
-            } else {
-
-                #Try to split the genera/species
-                genus_species<-check.split(matrix_taxon, Ref_list[[2]])
-
-                #If genus or genus_species is present, save the information
-                if(!is.na(genus_species[1])) {
-                    #Saving the information
-                    is_living<-TRUE
-                    is_source<-names(Ref_list)[2]
-
-                    #Checking the level
-                    if(!is.na(genus_species[2])) {
-                        is_level<-"Species"
-                    } else {
-                        is_level<-"Genus"
-                    }
-
-                } else {
-                    #Checking through the third element of the list (Paleo DB)
-                    is_match<-find.match(matrix_taxon, Ref_list[[3]])[1]
-
-                    if(!is.na(is_match)) {
-                        #Saving the information
-                        is_living<-FALSE
-                        is_source<-names(Ref_list)[3]
-
-                        #Checking the level
-                        if(level.column(is_match, Ref_list[[3]])==1) {
-                            #Present in the first column
-                            is_level<-"Genus"
-                        } else {
-                            #Present in the second column
-                            is_level<-"Species"
-                        }
-
-                    } else {
-                        #Try to split the genera/species
-                        genus_species<-check.split(matrix_taxon, Ref_list[[3]])[1]
-
-                        #If genus or genus_species is present, save the information
-                        if(!is.na(genus_species[1])) {
-                            #Saving the information
-                            is_living<-FALSE
-                            is_source<-names(Ref_list)[3]
-
-                            #Checking the level
-                            if(!is.na(genus_species[2])) {
-                                is_level<-"Species"
-                            } else {
-                                is_level<-"Genus"
-                            }
-
-                        } else {
-                            #No match at all!
-                            is_living<-NA
-                            is_source<-NA
-                            is_level<-NA
-                        } 
-                    }
-                }
-            }
-        }
-
-        #Output
-        return(c(is_living, is_source, is_level))
-    }
-
-    #DEBUG
-    matrix_taxon<-"Bob_the_builder" ; match.taxon(matrix_taxon, Ref_list) #Should be NAs !!
-    matrix_taxon<-"Tachyglossus_aculeatus" ; match.taxon(matrix_taxon, Ref_list)
-    matrix_taxon<-"T._aculeatus" ; match.taxon(matrix_taxon, Ref_list) #Should be NAs !!
-    matrix_taxon<-"Suidae" ; match.taxon(matrix_taxon, Ref_list)
-    matrix_taxon<-"Amphiperatherium_maximum" ; match.taxon(matrix_taxon, Ref_list)
-    matrix_taxon<-"Amphiperatherium" ; match.taxon(matrix_taxon, Ref_list)
-
-
-
-#End
+    return(output)
 }
-
-#Output table
-
-#matrix, taxon, characters, living, source, level
-#GS254, Homo_sapiens, 160, TRUE, tree, species
---
+++ Functions/test/extract.names.test.R
@@ -0,0 +1,94 @@
+#Fix data loading
+setwd("~/PhD/Projects/Missing_living_mammals/Functions")
+
+#Loading the functions
+sourceDir <- function(path, trace = TRUE, ...) {
+    for (nm in list.files(path, pattern = "[.][RrSsQq]$")) {
+    if(trace) cat(nm,":")
+        source(file.path(path, nm), ...)
+        if(trace) cat("\n")
+    }
+}
+sourceDir(".")
+
+#Testing
+library(testthat)
+setwd("test/")
+
+#Data loading
+matrix<-read.nexus.data("../../Data/Matrices/GL1988-GH.nex")
+tree_list<-read.table("../../Data/Taxon_References/FritzTree_taxa.txt", header=F, stringsAsFactors=F)
+WR_list<-read.csv("../../Data/Taxon_References/WilsonReederMSW.csv", header=T, stringsAsFactors=F)
+Paleo_list<-read.csv("../../Data/Taxon_References/pdb_taxa.csv", header=T, stringsAsFactors=F)
+Ref_list<-list(tree_list, WR_list, Paleo_list)
+names(Ref_list)<-c("tree", "WR", "Paleo")
+
+
+#Find match
+expect_is(find.match("Bob_the_builder", Ref_list[[1]]), 'logical') ; message('.', appendLF=FALSE)
+expect_false(find.match("Bob_the_builder", Ref_list[[1]])) ; message('.', appendLF=FALSE)
+expect_false(find.match("Bob_the_builder", Ref_list[[2]])) ; message('.', appendLF=FALSE)
+expect_false(find.match("Bob_the_builder", Ref_list[[3]])) ; message('.', appendLF=FALSE)
+
+expect_true(find.match("Tachyglossus_aculeatus", Ref_list[[1]])) ; message('.', appendLF=FALSE)
+expect_false(find.match("Tachyglossus_aculeatus", Ref_list[[2]])) ; message('.', appendLF=FALSE)
+expect_false(find.match("Tachyglossus_aculeatus", Ref_list[[3]])) ; message('.', appendLF=FALSE)
+
+expect_false(find.match("Suidae", Ref_list[[1]])) ; message('.', appendLF=FALSE)
+expect_true(find.match("Suidae", Ref_list[[2]])) ; message('.', appendLF=FALSE)
+expect_false(find.match("Suidae", Ref_list[[3]])) ; message('.', appendLF=FALSE)
+
+expect_false(find.match("Amphiperatherium", Ref_list[[1]])) ; message('.', appendLF=FALSE)
+expect_false(find.match("Amphiperatherium", Ref_list[[2]])) ; message('.', appendLF=FALSE)
+expect_true(find.match("Amphiperatherium", Ref_list[[3]])) ; message('.', appendLF=FALSE)
+
+#level.column
+is_match<-grep("Bob_the_builder", as.character(unlist(Ref_list[[1]])), ignore.case=TRUE, value=TRUE)[1]
+expect_equal(length(level.column(is_match, Ref_list[[1]])), 0) ; message('.', appendLF=FALSE)
+is_match<-grep("Bob_the_builder", as.character(unlist(Ref_list[[2]])), ignore.case=TRUE, value=TRUE)[1]
+expect_equal(length(level.column(is_match, Ref_list[[2]])), 0) ; message('.', appendLF=FALSE)
+is_match<-grep("Bob_the_builder", as.character(unlist(Ref_list[[3]])), ignore.case=TRUE, value=TRUE)[1]
+expect_equal(length(level.column(is_match, Ref_list[[3]])), 0) ; message('.', appendLF=FALSE)
+
+is_match<-grep("Tachyglossus_aculeatus", as.character(unlist(Ref_list[[1]])), ignore.case=TRUE, value=TRUE)[1]
+expect_equal(level.column(is_match, Ref_list[[1]]), 1) ; message('.', appendLF=FALSE)
+
+is_match<-grep("Suidae", as.character(unlist(Ref_list[[2]])), ignore.case=TRUE, value=TRUE)[1]
+expect_equal(level.column(is_match, Ref_list[[2]]), 5) ; message('.', appendLF=FALSE)
+
+is_match<-grep("Amphiperatherium", as.character(unlist(Ref_list[[3]])), ignore.case=TRUE, value=TRUE)[1]
+expect_equal(level.column(is_match, Ref_list[[3]]), 1) ; message('.', appendLF=FALSE)
+
+#check.split
+expect_equal(check.split("Bob@the@builder", Ref_list[[1]]), c(NA, NA))  ; message('.', appendLF=FALSE)
+expect_equal(check.split("Bob@the@builder", Ref_list[[1]]), c(NA, NA))  ; message('.', appendLF=FALSE)
+expect_equal(check.split("Bob@the@builder", Ref_list[[2]]), c(NA, NA)) ; message('.', appendLF=FALSE)
+expect_equal(check.split("Bob@the@builder", Ref_list[[3]]), c(NA, NA)) ; message('.', appendLF=FALSE)
+
+expect_equal(check.split("Bob_the_builder", Ref_list[[1]]), c(NA, NA)) ; message('.', appendLF=FALSE)
+expect_equal(check.split("Bob_the_builder", Ref_list[[2]]), c(NA, NA)) ; message('.', appendLF=FALSE)
+expect_equal(check.split("Bob_the_builder", Ref_list[[3]]), c(NA, NA)) ; message('.', appendLF=FALSE)
+
+expect_equal(check.split("Tachyglossus_aculeatus", Ref_list[[1]]), c(NA, NA)) ; message('.', appendLF=FALSE)
+expect_equal(check.split("Tachyglossus_aculeatus", Ref_list[[2]]), c("Tachyglossus", "aculeatus")) ; message('.', appendLF=FALSE)
+expect_equal(check.split("Tachyglossus_aculeatus", Ref_list[[3]]), c("Tachyglossus", "aculeatus")) ; message('.', appendLF=FALSE)
+
+expect_equal(check.split("Amphiperatherium_maximum", Ref_list[[1]]), c(NA, NA)) ; message('.', appendLF=FALSE)
+expect_equal(check.split("Amphiperatherium_maximum", Ref_list[[2]]), c(NA, NA)) ; message('.', appendLF=FALSE)
+expect_equal(check.split("Amphiperatherium_maximum", Ref_list[[3]]), c("Amphiperatherium", "maximum")) ; message('.', appendLF=FALSE)
+
+#match taxon
+expect_equal(match.taxon("Bob@the@builder", Ref_list), c(NA, NA, NA)) ; message('.', appendLF=FALSE)
+expect_equal(match.taxon("Bob_the_builder", Ref_list), c(NA, NA, NA)) ; message('.', appendLF=FALSE)
+expect_equal(match.taxon("Tachyglossus_aculeatus", Ref_list), c("TRUE","tree","Species")) ; message('.', appendLF=FALSE)
+expect_equal(match.taxon("T._aculeatus", Ref_list), c(NA, NA, NA)) ; message('.', appendLF=FALSE)
+expect_equal(match.taxon("Tachyglossus", Ref_list), c("TRUE","WR","Genus")) ; message('.', appendLF=FALSE)
+expect_equal(match.taxon("tACHYGLOSSUS", Ref_list), c("TRUE","WR","Genus")) ; message('.', appendLF=FALSE)
+expect_equal(match.taxon("aculeatus", Ref_list), c("TRUE","WR","Species")) ; message('.', appendLF=FALSE)
+expect_equal(match.taxon("Suidae", Ref_list), c("TRUE","WR","Family")) ; message('.', appendLF=FALSE)
+expect_equal(match.taxon("Amphiperatherium_maximum", Ref_list), c("FALSE","Paleo","Species")) ; message('.', appendLF=FALSE)
+expect_equal(match.taxon("Amphiperatherium", Ref_list), c("FALSE","Paleo","Genus")) ; message('.', appendLF=FALSE)
+
+#extract name
+matching_table<-extract.names(matrix, Ref_list)
+expect_is(matching_table, "data.frame") ; message('.', appendLF=FALSE)
