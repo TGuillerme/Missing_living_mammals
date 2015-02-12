faeef64f8bcdeec085aff4e3671d12f95c5c3cca
TGuillerme <guillert@tcd.ie>
2015-02-12 12:11:10 +0000

    Rarefaction analysis

--- Analysis/Rarefaction.R
+++ Analysis/Rarefaction.R
@@ -14,28 +14,12 @@ load("../Data/List_of_matching_taxa.Rda")
 extraction_table[which(extraction_table$Matrix == "GS2007-R.nex"), 3]<-88
 extraction_table[which(extraction_table$Matrix == "GS2010-VJD.nex"), 3]<-157
 extraction_table[which(extraction_table$Matrix == "GS2012-GM.nex"), 3]<-45
-extraction_table[which(extraction_table$Matrix == "GS2012-SM.nex"), 3]<-
+extraction_table[which(extraction_table$Matrix == "GS2012-SM.nex"), 3]<-157
 
-#Creating sub tables with a minimum number of characters
-#1 Character
-extraction_table1<-extraction_table[which(as.numeric(extraction_table$Characters) >= 1),]
-#50 Characters
-extraction_table50<-extraction_table[which(as.numeric(extraction_table$Characters) >= 50),]
-#100 Characters
-extraction_table100<-extraction_table[which(as.numeric(extraction_table$Characters) >= 100),]
-#150 Characters
-extraction_table150<-extraction_table[which(as.numeric(extraction_table$Characters) >= 150),]
-#200 Characters
-extraction_table200<-extraction_table[which(as.numeric(extraction_table$Characters) >= 200),]
-
-#Select the number of characters
-extraction_table<-extraction_table200
-
-#Selecting the living taxa (for a minimal number of characters)
+#Select the living taxa within the repositories sources (morphobank, Graeme Lloyd and Ross Mounce)
+#Living taxa
 living_taxa<-extraction_table[which(extraction_table$Living == TRUE),]
-living_taxa_list<-unique(living_taxa$Taxa)
 
-#Selecting the living taxa per data source
 #Morphobank
 morphobank1<-living_taxa[grep("MB", living_taxa$Matrix),]
 morphobank2<-living_taxa[grep("MP", living_taxa$Matrix),]
@@ -47,30 +31,59 @@ mounce<-living_taxa[grep("MM", living_taxa$Matrix),]
 #Graeme Lloyd
 lloyd<-living_taxa[grep("GL", living_taxa$Matrix),]
 
-#Google Scholar
-google<-living_taxa[grep("GS", living_taxa$Matrix),]
-
 #Combine the repositories data
 repositories<-rbind(morphobank, mounce, lloyd)
+#List of living taxa from the repositories
+repo_taxa<-unique(repositories$Taxa)
 
 
-#Extracting the number of living taxa from the repositories
-rep<-length(unique(repositories$Taxa))
-#Cumulative taxa addition per google search containing additional living taxa
-#Empty vector containing the number of taxa added
-taxa_added<-vector()
-for (mat in 1:length(levels(as.factor(google$Matrix)))) {
-    #Extracting a matrix
-    taxa_list<-google$Taxa[which(google$Matrix == levels(as.factor(google$Matrix))[mat])]
-    #Counting taxa that are not present in the repositories list already
-    taxa_unique<-length(which(is.na(match(taxa_list, repositories$Taxa))))
-    #Adding the number of taxa added to the taxa existing in the repositories
-    taxa_added[mat]<-taxa_unique
+#Loading the google search results
+Google_search<-read.csv("../Data/GSresults.csv")
+#Setting the matrix column as character
+Google_search$Counts<-as.character(Google_search$Matrix)
+
+#Replacing the non google results
+Google_search[grep("GS", Google_search$Counts, invert=TRUE),4]<-0
+#Select the GS matrices
+GS_matrices<-which(Google_search$Counts != 0)
+#Within each matrix, select the number of extra taxa
+
+#Selecting the living taxa in each Google scholar matrices
+taxa_present<-list()
+for (mat in 1:length(GS_matrices)) {
+    #Selecting the living taxa from the matrix
+    taxa_present[[mat]]<-living_taxa$Taxa[grep(Google_search$Counts[GS_matrices[mat]], living_taxa$Matrix)]
+    names(taxa_present)[mat]<-Google_search$Counts[GS_matrices[mat]]
 }
-taxa_added<-sort(taxa_added, decreasing=TRUE)
-taxa_add<-cumsum(taxa_added)
 
-#Select the repo sp and then add the google sources one by one.
+#Counting the number of living taxa per matrix
+number_of_living<-unlist(lapply(taxa_present, length))
 
-x<-c(10,11,12,13,14)
-barplot(taxa_add, xlab="Google scholar searchs", ylab="Number of unique living OTUs", names.arg=c(seq(1:length(taxa_add))))
+#Making a sub_table with the results
+sub_results<-data.frame("Matrix"=Google_search$Matrix[GS_matrices], "Google_search"=GS_matrices, "N_Living"=number_of_living, "Extra_taxa"=rep(0, length(GS_matrices)))
+sub_results<-sub_results[order(sub_results$N_Living, decreasing=TRUE),]
+
+#Per matrix, count the number of extra taxa that are not present in the repository AND in the former matrices (additive count)
+for (mat in 1:nrow(sub_results)) {
+    #Select non matching taxa
+    new_taxa<-taxa_present[[grep(sub_results$Matrix[mat], names(taxa_present))[1]]][which(is.na(match(taxa_present[[grep(sub_results$Matrix[mat], names(taxa_present))[1]]], repo_taxa)))]
+
+    #WARNING: this selects anything regardless typos or taxonomic level. Include a check?
+
+    #Counting the new taxa
+    sub_results[mat,4]<-length(new_taxa)
+
+    #Adding these new taxa to the repositories list
+    if (length(new_taxa) > 0) {
+        repo_taxa<-c(repo_taxa, new_taxa)
+    }
+}
+
+#Replacing the Extra taxa in the Google_search results
+for (mat in 1:nrow(sub_results)) {
+    Google_search$Counts[sub_results$Google_search[mat]]<-sub_results$Extra_taxa[mat]
+}
+
+Additional_data<-sort(as.numeric(Google_search$Counts), decreasing=TRUE)
+
+plot(cumsum(Additional_data), type="l", ylab="Number of additional OTUs", xlab="Google Scholars papers")
